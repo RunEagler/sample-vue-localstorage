4cdf772fdf4edf8f0badbfc050b2b339c75f660b
RunEagler <ohk.cotf1649@gmail.com>
2018-07-07 10:43:08 +0900

    add: project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div id="container">
+      <LocalStorageEntry></LocalStorageEntry>
+      <LocalStorageGet></LocalStorageGet>
+    </div>
+  </div>
+</template>
+
+<script>
+import LocalStorageEntry from './components/LocalStorageEntry'
+import LocalStorageGet from './components/LocalStorageGet'
+
+export default {
+  name: 'App',
+  components: {
+    LocalStorageEntry,
+    LocalStorageGet
+  }
+}
+</script>
+
+<style>
+
+  #container {
+    display: grid;
+    grid-template-columns: 500px 1fr;
+  }
+</style>
